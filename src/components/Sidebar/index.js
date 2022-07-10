061cf16d5da854f67ec63fbac809a35834869b5f
Emilie <milogare@yahoo.fr>
2022-07-11 01:50:14 +0200

    Added social networks links to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,12 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faEnvelope,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faGitlab, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -32,6 +37,35 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/watch?v=bmpI252DmiI"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/watch?v=bmpI252DmiI"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/watch?v=bmpI252DmiI"
+        >
+          <FontAwesomeIcon icon={faGitlab} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
